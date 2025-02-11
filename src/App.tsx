595cfed60561c30205a93c3498cf947c6ec766e0
nyannncatt <kamirelax187@gmail.com>
2025-02-11 08:40:36 +0800

    added major changes on route

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -32,7 +31,7 @@ import '@ionic/react/css/palettes/dark.system.css';
 
 /* Theme variables */
 import './theme/variables.css';
-import Login from './pages/Login';
+import Login from './pages/login';
 import Menu from './pages/Menu';
 
 setupIonicReact();
@@ -41,10 +40,8 @@ const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
-
-      <Route exact path="/it35-lab" component={Login} />
-      <Route exact path="/it35-lab/app" component={Menu} />
-
+        <Route exact path="/it35-lab" component={Login} />
+        <Route path="/it35-lab/app" component={Menu} />
       </IonRouterOutlet>
     </IonReactRouter>
   </IonApp>
