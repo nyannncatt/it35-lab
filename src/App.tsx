a6922a331af93787eb027ed528639d1d9d3de43a
nyannncatt <kamirelax187@gmail.com>
2025-02-11 22:30:59 +0800

    route fix and final success page deployment

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ import '@ionic/react/css/palettes/dark.system.css';
 
 /* Theme variables */
 import './theme/variables.css';
-import Login from './pages/login';
+import Login from './pages/Login';
 import Menu from './pages/Menu';
 
 setupIonicReact();
