dc9ba26ebe1686b2d2e4f6e392213b42bbedb694
nyannncatt <kamirelax187@gmail.com>
2025-02-26 07:51:00 +0800

    Updates

--- assets/ios.transition-legacy-04APZS8w.js
+++ assets/ios.transition-legacy-04APZS8w.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CLL8JxIo.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-DbP1OwIg.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-hBHX0hCy.js
+++ assets/swipe-back-legacy-hBHX0hCy.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CLL8JxIo.js","./index3-legacy-CctikQOA.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-DbP1OwIg.js","./index3-legacy-E73SDCzl.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
