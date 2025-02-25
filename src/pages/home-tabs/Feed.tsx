c2fe4b6b08bdfa76b5c9dcb2912240dfa9c7556c
nyannncatt <kamirelax187@gmail.com>
2025-02-25 08:52:17 +0800

    added ionic list

--- src/pages/home-tabs/Feed.tsx
+++ src/pages/home-tabs/Feed.tsx
@@ -1,38 +1,50 @@
 import { 
-    IonButtons,
-      IonContent, 
-      IonHeader, 
-      IonMenuButton, 
-      IonPage, 
-      IonTitle, 
-      IonToolbar 
-  } from '@ionic/react';
+  IonButtons, 
+  IonContent, 
+  IonHeader, 
+  IonMenuButton, 
+  IonPage, 
+  IonTitle, 
+  IonToolbar, 
+  IonList, 
+  IonItem, 
+  IonLabel 
+} from '@ionic/react';
+
+const Feed: React.FC = () => {
   
-  const Feed: React.FC = () => {
-    return (
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonButtons slot='start'>
-              <IonMenuButton></IonMenuButton>
-            </IonButtons>
-            <IonTitle>Feed</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent fullscreen>
-        <div
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            height: '100%',
-          }}
-        >
-          Feed
-        </div>
-        </IonContent>
-      </IonPage>
-    );
-  };
-  
-  export default Feed;
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonMenuButton />
+          </IonButtons>
+          <IonTitle>Feed</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+
+      <IonContent color="light">
+        <IonList inset={true}>
+          <IonItem>
+            <IonLabel>Pokémon Yellow</IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>Mega Man X</IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>The Legend of Zelda</IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>Pac-Man</IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>Super Mario World</IonLabel>
+          </IonItem>
+        </IonList>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Feed;
