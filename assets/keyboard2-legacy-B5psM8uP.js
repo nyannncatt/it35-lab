111af934f3bed6338c52beb004c661f53026b724
nyannncatt <kamirelax187@gmail.com>
2025-02-26 20:11:46 +0800

    Updates

--- assets/keyboard2-legacy-B5psM8uP.js
+++ assets/keyboard2-legacy-B5psM8uP.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DbP1OwIg.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-CEbmpxSQ.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
