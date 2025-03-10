ed7019b3fb392d317816801d8867ad8be02d89d9
nyannncatt <kamirelax187@gmail.com>
2025-03-11 00:11:40 +0800

    Updates

--- assets/keyboard2-legacy-CWIhZXoc.js
+++ assets/keyboard2-legacy-CWIhZXoc.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CD0dKWmK.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-XaCMnGaq.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-DUTcbBoD.js
+++ assets/swipe-back-legacy-DUTcbBoD.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CD0dKWmK.js","./index3-legacy-HEDiwZa6.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-XaCMnGaq.js","./index3-legacy-Dy4FhQ75.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
