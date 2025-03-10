1d1d65776e782225d1db1d1b81faab8f791698f4
nyannncatt <kamirelax187@gmail.com>
2025-03-10 21:59:45 +0800

    Updates

--- assets/swipe-back-legacy-DLOGFlHI.js
+++ assets/swipe-back-legacy-DLOGFlHI.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-D7uvUE5T.js","./index3-legacy-f-cxSmI7.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-CqSPeiC9.js","./index3-legacy-6mgOgGe9.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
