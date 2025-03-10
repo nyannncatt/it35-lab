4dadce07f0c6eec51d2dcc0233040d0891721e1a
nyannncatt <kamirelax187@gmail.com>
2025-03-10 21:59:26 +0800

    UI changed and added registration text

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -6,48 +6,64 @@ import {
   IonIcon,
   IonInput,
   IonItem,
+  IonLabel,
   IonMenuButton,
   IonPage,
   IonTitle,
   IonToolbar,
   useIonRouter
 } from '@ionic/react';
-import { easelOutline, keyOutline, personCircleOutline, tvOutline } from 'ionicons/icons';
+import { keyOutline, personCircleOutline, tvOutline } from 'ionicons/icons';
+
 function Login() {
   const navigation = useIonRouter();
+
   const doLogin = () => {
     navigation.push('/it35-lab/app', 'forward', 'replace');
   };
+
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar>
+        <IonToolbar color="primary">
           <IonButtons slot='start'>
-            <IonMenuButton></IonMenuButton>
+            <IonMenuButton />
           </IonButtons>
           <IonTitle>Login</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent className='ion-padding'>
-        <div style={{ textAlign: 'center', marginBottom: '20px' }}>
-          <IonIcon icon={tvOutline} style={{ fontSize: '70px' }} />
+      <IonContent className='ion-padding' fullscreen>
+        <div style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: '40vh'
+        }}>
+          <IonIcon icon={tvOutline} style={{ fontSize: '80px', color: '#3880ff' }} />
+          <h2 style={{ marginTop: '10px', color: '#3880ff' }}>Welcome Back!</h2>
         </div>
-        <IonItem>
-        
-          <IonIcon icon={personCircleOutline} style={{ fontSize: '30px' }} />
-      
-          <IonInput label="" placeholder="Enter Username"></IonInput>
+
+        <IonItem lines="full">
+          <IonIcon icon={personCircleOutline} slot="start" style={{ fontSize: '24px', color: '#3880ff' }} />
+          <IonInput label="" placeholder="Enter Username" clearInput></IonInput>
         </IonItem>
-        <IonItem>
-        <IonIcon icon={keyOutline} style={{ fontSize: '30px' }} />
-      
-          <IonInput label="" type="password" placeholder="Enter Password"></IonInput>
+
+        <IonItem lines="full">
+          <IonIcon icon={keyOutline} slot="start" style={{ fontSize: '24px', color: '#3880ff' }} />
+          <IonInput label="" type="password" placeholder="Enter Password" clearOnEdit></IonInput>
         </IonItem>
-        <IonButton onClick={() => doLogin()} expand="full">
+
+        <IonButton onClick={doLogin} expand="block" shape="round" color="primary" style={{ marginTop: '20px' }}>
           Login
         </IonButton>
+
+        <div style={{ textAlign: 'center', marginTop: '15px' }}>
+          <p style={{ color: '#666' }}>No account? Igotchu bro <a href="#" style={{ color: '#3880ff' }}>Register here</a></p>
+        </div>
       </IonContent>
     </IonPage>
   );
 }
+
 export default Login;
