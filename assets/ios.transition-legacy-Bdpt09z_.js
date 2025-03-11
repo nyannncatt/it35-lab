ba75842d2f7056222ff411c6a1c389d7a76569f4
nyannncatt <kamirelax187@gmail.com>
2025-03-11 20:32:57 +0800

    Updates

--- assets/ios.transition-legacy-Bdpt09z_.js
+++ assets/ios.transition-legacy-Bdpt09z_.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-B_hxHP68.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-tUNSVZOW.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
