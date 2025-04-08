4399518517b5baa1a42a7577f15eaec99f10632f
nyannncatt <kamirelax187@gmail.com>
2025-04-08 08:47:33 +0800

    Updates

--- assets/md.transition-legacy-CZzI60bk.js
+++ assets/md.transition-legacy-CZzI60bk.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BDqQN-be.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-DrcxM5d-.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
