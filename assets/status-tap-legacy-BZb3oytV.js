23abb4d2fbaa34e096e301af557e01f97d05089e
nyannncatt <kamirelax187@gmail.com>
2025-04-22 10:38:06 +0800

    Updates

--- assets/status-tap-legacy-BZb3oytV.js
+++ assets/status-tap-legacy-BZb3oytV.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-thDqvtTV.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
+System.register(["./index-legacy-DFTzp_iH.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
