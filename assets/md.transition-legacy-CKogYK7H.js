40cbbed06a475b738ee979c9f6123c80172d1508
nyannncatt <kamirelax187@gmail.com>
2025-05-08 20:17:06 +0800

    Updates

--- assets/md.transition-legacy-CKogYK7H.js
+++ assets/md.transition-legacy-CKogYK7H.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CWSLv5Ho.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-DVxM7F_z.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
