c02777d420c4e180b123572d60afe4453a870401
nyannncatt <kamirelax187@gmail.com>
2025-05-09 06:31:39 +0800

    Updates

--- assets/ios.transition-legacy-D4XGqyg1.js
+++ assets/ios.transition-legacy-D4XGqyg1.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DVxM7F_z.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-CWSLv5Ho.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
